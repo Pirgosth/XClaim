2e0f7576c8482cf3d032487b2738a76a03e8feaf
Pirgosth <pierre.dedrie@epitech.eu>
2022-01-15 01:37:26 +0100

    Try some new implementation based on nearest point instead of center because of collision issues.

--- src/main/java/io/github/pirgosth/xclaim/math/CuboidRegion.java
+++ src/main/java/io/github/pirgosth/xclaim/math/CuboidRegion.java
@@ -9,10 +9,7 @@ import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.util.BlockVector;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 public class CuboidRegion extends Region implements ConfigurationSerializable {
 
@@ -23,15 +20,18 @@ public class CuboidRegion extends Region implements ConfigurationSerializable {
     @NotNull
     @Getter
     private final BlockVector highCorner;
+    private final int shortestDistance;
 
     public CuboidRegion(Map<String, Object> map) {
         Object rawWorldId = map.get("world-id");
         Object rawLowCorner = map.get("low-corner");
         Object rawHighCorner = map.get("high-corner");
+        Object rawDistance = map.get("distance");
 
         this.world = (rawWorldId instanceof String) ? Bukkit.getWorld(UUID.fromString((String) rawWorldId)) : null;
         this.lowCorner = (rawLowCorner instanceof Map) ? BlockVector.deserialize(SerializationUtils.safeMapSerialize((Map<?, ?>) rawLowCorner)) : null;
         this.highCorner = (rawHighCorner instanceof Map) ? BlockVector.deserialize(SerializationUtils.safeMapSerialize((Map<?, ?>) rawHighCorner)) : null;
+        this.shortestDistance = (rawDistance instanceof Integer) ? (Integer) rawDistance : null;
         if (this.lowCorner == null) throw new IllegalArgumentException("Low corner is null.");
         if (this.highCorner == null) throw new IllegalArgumentException("High corner is null.");
     }
@@ -40,6 +40,7 @@ public class CuboidRegion extends Region implements ConfigurationSerializable {
         world = Objects.requireNonNull(center.getWorld());
         lowCorner = new BlockVector(center.getX() - radius, world.getMinHeight(), center.getZ() - radius);
         highCorner = new BlockVector(center.getX() + radius, world.getMaxHeight(), center.getZ() + radius);
+        shortestDistance = this.getNearestBlockDist();
     }
 
     public boolean contains(Location location) {
@@ -64,10 +65,38 @@ public class CuboidRegion extends Region implements ConfigurationSerializable {
         return (highCorner.getBlockX() - lowCorner.getBlockX()) / 2;
     }
 
+    private List<BlockVector> getPerimeterBlocks() {
+        List<BlockVector> result = new ArrayList<>();
+        for (int i = 0; i <= this.getRadius()*2; i++) {
+            result.add(new BlockVector(this.lowCorner.getBlockX() + i, 0, this.lowCorner.getBlockZ()));
+            result.add(new BlockVector(this.lowCorner.getBlockX() + i, 0, this.highCorner.getBlockZ()));
+            if (i > 0 && i < this.getRadius()*2) {
+                result.add(new BlockVector(this.lowCorner.getBlockX(), 0, this.lowCorner.getBlockZ() + i));
+                result.add(new BlockVector(this.highCorner.getBlockX(), 0, this.lowCorner.getBlockZ() + i));
+            }
+        }
+
+        return result;
+    }
+
+    public int getNearestBlockDist() {
+        if (this.contains(new Location(this.world, 0, 0, 0))) return 0;
+
+        List<BlockVector> perimeter = this.getPerimeterBlocks();
+        BlockVector firstBlock = perimeter.get(0);
+        int minDist = (int) Math.ceil(Math.sqrt(firstBlock.getBlockX() * firstBlock.getBlockX() + firstBlock.getBlockZ() * firstBlock.getBlockZ()));
+        for (BlockVector block : perimeter) {
+            int distance = (int) Math.ceil(Math.sqrt(block.getBlockX() * block.getBlockX() + block.getBlockZ() * block.getBlockZ()));
+            if (minDist > distance) {
+                minDist = distance;
+            }
+        }
+
+        return minDist;
+    }
+
     public int distance() {
-        int x = (this.highCorner.getBlockX() + this.lowCorner.getBlockX()) / 2;
-        int z = (this.highCorner.getBlockZ() + this.lowCorner.getBlockZ()) / 2;
-        return (int) Math.ceil(Math.sqrt(x*x + z*z));
+        return shortestDistance;
     }
 
     @NotNull
@@ -77,6 +106,7 @@ public class CuboidRegion extends Region implements ConfigurationSerializable {
         map.put("world-id", this.world.getUID().toString());
         map.put("low-corner", this.lowCorner.serialize());
         map.put("high-corner", this.highCorner.serialize());
+        map.put("distance", this.shortestDistance);
 
         return map;
     }
